ed9f1474a705cf2a354dcfe830a1091b90b6ec6a
Laurent <laurent.phi@gmail.com>
2020-04-23 12:16:04 +0200

    add layout

--- src/main/java/com/origaminormandy/RestoConfiguration.java
+++ src/main/java/com/origaminormandy/RestoConfiguration.java
@@ -1,35 +1,13 @@
 package com.origaminormandy;
 
-import java.time.LocalTime;
-import java.time.OffsetTime;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-import org.springframework.format.support.DefaultFormattingConversionService;
-import org.springframework.format.support.FormattingConversionService;
 
 @Configuration
 public class RestoConfiguration {
 	
-	@Bean
-	public FormattingConversionService conversionService() {
-		System.out.println("here");
-		DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService(false);
 
-		DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
-		registrar.setDateFormatter(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-		registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
-		registrar.registerFormatters(conversionService);
-
-		// other desired formatters
-		
-		
 	
 
-		return conversionService;
-	}
 }
+
+
