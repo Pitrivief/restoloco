ef11cc6e0e1f1950f0329593120a5496c70e3381
Laurent <laurent.phi@gmail.com>
2020-05-02 22:08:50 +0200

    bug admin form + clear phone data + add button

--- src/main/java/com/origaminormandy/resto/RestoRepositoryImpl.java
+++ src/main/java/com/origaminormandy/resto/RestoRepositoryImpl.java
@@ -14,7 +14,6 @@ import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.attoparser.ParsingProcessingInstructionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
