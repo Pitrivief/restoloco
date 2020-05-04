98b8b36d17ca50800ce30d148ec90c43a4b01a28
Laurent <laurent.phi@gmail.com>
2020-05-04 18:25:09 +0200

    Add created date and modified date for contact

--- src/main/java/com/origaminormandy/contact/Contact.java
+++ src/main/java/com/origaminormandy/contact/Contact.java
@@ -1,61 +1,88 @@
 package com.origaminormandy.contact;
 
+
+
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Contact {
 
-	
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-     
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
 	private String name;
 	private String email;
 	private String phone;
 	private String text;
 	private String companyType;
+
+	@Column(name = "created_date", updatable = false)
+	@CreatedDate
+	private Date createdDate;
+
+	@Column(name = "modified_date")
+	@LastModifiedDate
+	private Date modifiedDate;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 	public String getCompanyType() {
 		return companyType;
 	}
+
 	public void setCompanyType(String companyType) {
 		this.companyType = companyType;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
-	
-	
-	
+
 }
