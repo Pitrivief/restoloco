bed4451e96f1361506b1c18bac2e283a2e0bed96
Laurent <laurent.phi@gmail.com>
2020-05-07 22:57:28 +0200

    Correct deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 yarn encore prod
 mvn install
-app_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version)
-docker save resto:0.0.2 | bzip2 | pv | ssh root@restoloco.fr 'bunzip2 | docker load'  
-cat deploy/docker-compose-traefik.yaml | sed -e "s/{{version}}/$app_version/" | ssh root@restoloco.fr 'echo >> deploy/docker-compose-traefik.yaml'
+app_version=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -ve "^\[INFO\]"`
+echo "APP_VERSION=$app_version"
+docker save resto:$app_version | bzip2 | pv | ssh root@restoloco.fr 'bunzip2 | docker load'  
+cat deploy/docker-compose-traefik.yaml | sed -e "s/resto:{{version}}/resto:$app_version/" | ssh root@restoloco.fr 'echo >> deploy/docker-compose-traefik.yaml'
 ssh root@restoloco.fr docker-compose -f deploy/docker-compose-traefik.yaml up -d
 
