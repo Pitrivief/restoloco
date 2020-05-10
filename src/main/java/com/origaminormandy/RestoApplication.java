8f0f1ff8ba61610ae704cff005b0d3cb33ea5c45
Thomas Ripoll <t-ripoll@legallais.com>
2020-05-10 10:40:11 +0200

    arrow fixes 	modified:   src/main/java/com/origaminormandy/RestoApplication.java 	modified:   src/main/resources/assets/scss/Layout.scss 	modified:   src/main/resources/templates/footer.html

--- src/main/java/com/origaminormandy/RestoApplication.java
+++ src/main/java/com/origaminormandy/RestoApplication.java
@@ -1,5 +1,7 @@
 package com.origaminormandy;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +13,11 @@ public class RestoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestoApplication.class, args);
 	}
+        
+        @PostConstruct
+        void started() {
+            TimeZone.setDefault(TimeZone.getTimeZone("Europe/Paris"));
+        }
 
 	
 }
